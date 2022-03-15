859a6a5f0fe1f70794b1e36206f94a4c09ae783f
aldanelva <aldamondel@gmail.com>
2022-03-15 10:12:44 +0700

    welcome and web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
             <ul class="list-unstyled components">
 
                 <li class="active"> <a href="index.html">Home</a></li>
-                 <li> <a href="about.html">About</a></li>
+                 <li> <a href="about.blade.php">About</a></li>
                                         <li> <a href="product.html">Product</a></li>
                                         <li> <a href="blog.html">Blog</a></li>
                                         <li> <a href="contact.html">Contact us</a></li>
