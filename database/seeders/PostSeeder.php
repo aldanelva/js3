eb5d13dca2c94052dcb7b59d38e42816843668bf
aldanelva <aldamondel@gmail.com>
2022-03-15 18:11:14 +0700

    seeeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'name' => 'Produk 1',
+            'slug' => 'produk-1',
+            'image' => 'https://via.placeholder.com/640x480.png/00ff77?text=Produk%201',
+            'price' => '20000'
+        ]);
+    }
+}
